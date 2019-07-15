a4b789a7b9cedb6f9ad2a875d82643a5777c7e6b
Steve McGee <solodesign31@gmail.com>
2019-07-14 23:44:26 -0600

    fix: location link and wording

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -6,10 +6,10 @@ export default () => {
     <Card style={ { padding: 12, margin: 6 } }>
       <Typography variant="h4">Campout Info</Typography>
       <CardContent style={ { marginLeft: 12 } }>
-        <p>Camp Helaman will be for all young men in the Stake and all deacons, teachers and priests are invited to participate.  This 3-day camp out will replace the stake scout camp for 2019. The location details for this campout is found at the bottom of this page.</p>
+        <p>Camp Helaman will be for all young men in the Stake and all deacons, teachers and priests are invited to participate.  This 3-day camp out will replace the Stake Scout Camp for 2019. The location details found <a href="#location">here</a>.</p>
         <p>We will be leaving at 5:00 PM on Wednesday the 17th of July and returning Saturday the 20th.</p>
         <p>On Wednesday we will arrive and set up camp, and the Stake will provide dinner that night.</p>
-        <p>Thursday and Friday we will be cooking all our own meals but the Stake will be in charge of all activities.</p>
+        <p>Thursday and Friday we will be cooking all our own meals but the Stake will be in charge of all activities. For the campsite the ward has all the cooking stoves, pots and pans, tents, and mess kits</p>
         <p>Saturday, the Stake will provide the breakfast, then we will break camp and return home. We should arrive back around noon.</p>
         <p>Some of the activities may include Archery, Pioneering course or competition, Fire building skills, Wilderness survival classes (and have the boys build a shelter to sleep in one night), First Aid, Hiking, Knots, and fun team-building activities</p>
         <p>Please leave your electronics at home... Any problems please talk to your leaders.</p>
--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Card, CardContent } from '@material-ui/core'
+import { Card, CardContent, Typography } from '@material-ui/core'
 
 export default () => {
   return (
     <Card style={ { padding: 12, margin: 6 } }>
+      <Typography variant="h4">Location</Typography>
       <CardContent>
-        <p>This campout will be held at Camp Eagle Mountain.</p>
+        <p id="location">This campout will be held at <a href="https://www.google.com/maps/place/Camp+Eagle+Mountain/@40.305697,-112.1158509,17z/data=!3m1!4b1!4m5!3m4!1s0x874d636c4c35ef01:0x487de3df7a98064f!8m2!3d40.305697!4d-112.1136622" target="_blank" rel="noopener noreferrer">Camp Eagle Mountain</a>.</p>
         <p>Location directions: Traveling south on UT 73, after the town of Cedar Fort, there's a turn off heading west at the Eagle Mountain City limit sign. Look for the white "CAMP EAGLE MOUNTAIN" sign. This road leads directly to Camp Eagle Mountain. If you pass the suburb of White Hills, you have gone too far.</p>
         <p>Alternatively you can paste this location code into Google Maps: 8V3F+HF</p>
       </CardContent>
