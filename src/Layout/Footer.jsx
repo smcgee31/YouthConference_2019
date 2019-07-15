2caef0d85267308694c517b3e66e148fcf4c313d
Steve McGee <solodesign31@gmail.com>
2019-07-14 18:51:41 -0600

    fix: add directions to footer

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -4,8 +4,10 @@ import { Card, CardContent } from '@material-ui/core'
 export default () => {
   return (
     <Card style={ { padding: 12, margin: 6 } }>
-      <CardContent style={ { color: 'red', textAlign: 'center', fontSize: 24 } }>
-        Please leave your electronics at home... Any problems please talk to your leaders.
+      <CardContent>
+        <p>This campout will be held at Camp Eagle Mountain.</p>
+        <p>Location directions: Traveling south on UT 73, after the town of Cedar Fort, there's a turn off heading west at the Eagle Mountain City limit sign. Look for the white "CAMP EAGLE MOUNTAIN" sign. This road leads directly to Camp Eagle Mountain. If you pass the suburb of White Hills, you have gone too far.</p>
+        <p>Alternatively you can paste this location code into Google Maps: 8V3F+HF</p>
       </CardContent>
     </Card>
   )
