6b216ed8143f6432142e8aa31aaefafa6321f482
Steve McGee <solodesign31@gmail.com>
2021-06-13 20:09:22 -0600

    update general info

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -4,29 +4,33 @@ import { Card, CardContent, Typography } from '@material-ui/core';
 export default () => {
   return (
     <Card style={{ padding: 12, margin: 6 }}>
-      <Typography variant="h4">Camp Info</Typography>
+      <Typography variant="h4">General Camp Info</Typography>
       <CardContent style={{ marginLeft: 12 }}>
         <p>
           This camp will be for all young men in the Stake. All deacons, teachers and priests are
           invited to participate. The location details found <a href="#location">here</a>.
         </p>
         <p>
-          We will be leaving at 5:00 PM on Wednesday the XXXX of July and returning Saturday the
-          XXXX.
+          We will be meeting at the church at 5:00 PM on Wednesday the 23rd of June and returning
+          Saturday the 26th around noon.
         </p>
         <p>
-          On Wednesday we will arrive and set up camp, and the Stake will provide dinner that night.
+          The Stake will provide dinner on Wednesday evening and Saturday morning. All other meals
+          will be by each Ward on their own.
         </p>
         <p>
-          Thursday and Friday each Ward will be cooking their own meals but the Stake will be in
-          charge of all activities. Each Ward will also provide or arrange for all the camping gear
-          requirements of their Ward.
+          There is a schedule of events that includes some "free time" that the boys were interested
+          in but also plenty of spiritual time as a group that they were also interested in.
         </p>
         <p>
           Saturday, the Stake will provide the breakfast, then we will break camp and return home.
           We should arrive back early afternoon.
         </p>
-        <p>If each Ward could bring as much water as possible that would be a great help!</p>
+        <p>
+          Water is an issue this year. There are spigots in the parks but it is preferred that
+          campers try to provide their own water. So, if each Ward could bring as much water as
+          possible that would be a great help!
+        </p>
       </CardContent>
     </Card>
   );
