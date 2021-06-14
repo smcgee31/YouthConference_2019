1b6948a45ca54159a968c54ab15f51c88d1a9cba
Steve McGee <solodesign31@gmail.com>
2021-06-13 20:45:45 -0600

    update schedule

--- src/Components/Schedule.jsx
+++ src/Components/Schedule.jsx
@@ -8,12 +8,10 @@ export default () => {
         <Typography variant="h4">Schedule</Typography>
         <p>
           The Young Men have many activities and many times of personal free time. These activities
-          and times could change. Fishing and swimming will be available. If you'd like to fish
-          you'll need to bring your license, pole, bait, and tackle.
+          and times could change. Fishing and swimming will be available. Anyone who would like to
+          fish will need to bring their fishing license, pole, bait, and tackle.
         </p>
         <CardContent style={{ marginLeft: 12 }}>
-          <br />
-
           <div style={{ marginBottom: 48 }}>
             <Typography variant="h6">
               <strong>Wednesday</strong>
@@ -29,37 +27,45 @@ export default () => {
             <Typography variant="h6">
               <strong>Thursday</strong>
             </Typography>
-            <Typography>Morning Devotional: Provided XXXX</Typography>
-            <Typography>Breakfast: Provided by each Ward</Typography>
-            <Typography>Activity: XXXXX</Typography>
-            <Typography>Free time: XXXX</Typography>
-            <Typography>Lunch: Provided by each Ward</Typography>
-            <Typography>Activity: XXXXX</Typography>
-            <Typography>Free time: XXXX</Typography>
-            <Typography>Dinner: Provided by each Ward. Devotional to follow dinner</Typography>
+            <Typography>9:00 AM: Hike Left Fork Days Canyon Trail</Typography>
+            <Typography>10:00 AM: Scavenger hunt during hike (details to be provided)</Typography>
+            <Typography>12:00 PM: Lunch</Typography>
+            <Typography>
+              1:00 PM: Scripture Study class and Spiritual and Business life balance
+            </Typography>
+            <Typography>2:00 PM: Down time for games at camp</Typography>
+            <Typography>5:00 PM: TikTok Skit Challenge (Theme: New Perspectives)</Typography>
+            <Typography>6:00 PM: Dinner</Typography>
+            <Typography>7:00 PM: Capture the Flag at Jolley's Park</Typography>
+            <Typography>8:00 PM: Fireside by Bro. Chamberlin</Typography>
           </div>
 
           <div style={{ marginBottom: 48 }}>
             <Typography variant="h6">
               <strong>Friday</strong>
             </Typography>
-            <Typography>Morning Devotional: Provided XXXX</Typography>
-            <Typography>Breakfast: Provided by each Ward</Typography>
-            <Typography>Activity: XXXXX</Typography>
-            <Typography>Free time: XXXX</Typography>
-            <Typography>Lunch: Provided by each Ward</Typography>
-            <Typography>Activity: XXXXX</Typography>
-            <Typography>Free time: XXXX</Typography>
-            <Typography>Dinner: Provided by each Ward. Devotional to follow dinner</Typography>
+            <Typography>9:00 AM: Fishing & Swimming at Bartholomew park</Typography>
+            <Typography>12:00 PM: Lunch</Typography>
+            <Typography>
+              1:00 PM: Service Project (Cleaning up the Police Shooting Range in Hobble Creek
+              Canyon)
+            </Typography>
+            <Typography>
+              2:00 PM: Games at Jolly's Park (soccer, frisbee golf, ultimate frisbee)
+            </Typography>
+            <Typography>3:00 PM: Water fight</Typography>
+            <Typography>5:00 PM: President Nelson Challenge</Typography>
+            <Typography>6:00 PM: Dinner</Typography>
+            <Typography>8:00 PM: Testimony Meeting</Typography>
           </div>
 
           <div style={{ marginBottom: 48 }}>
             <Typography variant="h6">
               <strong>Saturday</strong>
             </Typography>
-            <Typography>Morning Devotional: Provided XXXX</Typography>
-            <Typography>Breakfast: Provided by the Stake</Typography>
-            <Typography>Break Camp and head home</Typography>
+            <Typography>9:00 AM: Breakfast provided by the Stake</Typography>
+            <Typography>10:00 AM: Break Camp (cleaner than we found it)</Typography>
+            <Typography>12:00 PM: Head home</Typography>
           </div>
         </CardContent>
       </Card>
