a06e7e77272fe849ee6846a6a89f849ab5e89d0b
Steve McGee <solodesign31@gmail.com>
2021-06-19 21:55:34 -0600

    update background

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import camping_bkgnd from './assets/camping_bkgnd.jpeg';
 
 import Header from './Layout/Header';
 import Main from './Layout/Main';
@@ -6,11 +7,29 @@ import Footer from './Layout/Footer';
 
 function App() {
   return (
-    <div style={ { backgroundColor: '#f5f5f5', padding: 12, maxWidth: 1028, margin: 'auto' } }>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <>
+      <div
+        style={{
+          backgroundImage: `url(${camping_bkgnd})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        hi
+        <div
+          style={{
+            backgroundColor: 'rgba(245,245,245,0.4)',
+            maxWidth: 1028,
+            padding: 12,
+            margin: 'auto',
+          }}
+        >
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      </div>
+    </>
   );
 }
 
